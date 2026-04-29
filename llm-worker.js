dbf24ec20f7fb1342ec09d667133698930d2f9ce
darrellpoe06 <47160366+darrellpoe06@users.noreply.github.com>
2026-04-29 12:17:13 -0500

    Update llm-worker.js

--- llm-worker.js
+++ llm-worker.js
@@ -1,4 +1,4 @@
-import { WebWorkerMLCEngineHandler } from '@mlc-ai/web-llm';
+import { WebWorkerMLCEngineHandler } from 'https://esm.run/@mlc-ai/web-llm';
 
 // Initialize the message handler to connect the Web Worker to the main app thread
 const handler = new WebWorkerMLCEngineHandler();
